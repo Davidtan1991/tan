0b53cb9410ae9fe6b1ea22b159ecd2633e00b902
FinnGlobal <finn.he0102@gmail.com>
2023-10-12 23:03:37 +1300

    uncompleted feedback datapart

--- controller/feedBackPage.py
+++ controller/feedBackPage.py
@@ -1,9 +1,14 @@
 from flask import Blueprint
 from flask.templating import render_template
+from func.data_Func import DataFunc
+
 
 
 feedBack_Page = Blueprint("feedBack_Page", __name__, template_folder = "templates")
+dataInstance = DataFunc()
 
 @feedBack_Page.route('/feedBack_Page')
 def feedBackPageFunc():
+    dataInstance.getFeedbackData()
+
     return render_template("feedBackPage.html")
