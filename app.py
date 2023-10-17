19d0181346eedafb5207cd07e693be65170f6dc9
FinnGlobal <finn.he0102@gmail.com>
2023-10-17 22:40:04 +1300

    relative path study

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from controller import homepage, bookingPage, feedBackPage
 
-
 app = Flask(__name__)
 
 
--- func/data_Func.py
+++ func/data_Func.py
@@ -2,14 +2,14 @@
 
 class DataFunc:
     def __init__(self) -> None:
-        self.feedBackpath = "../static/doc/SimulateDatabase_feedback.txt" #"../tan/static/doc/SimulateDatabase_feedback.txt"
-
-    def getFeedbackData(self):
+        self.feedBackpath = "./static/doc/SimulateDatabase_feedback.txt" #"../tan/static/doc/SimulateDatabase_feedback.txt"
+        
+    def getFeedbackData(self):        
         with open(self.feedBackpath, "r") as file_f:
             originalFeedbackData = file_f.readlines()
             file_f.close()
-        print(originalFeedbackData)????????????
-            
+
+
     
     def updateFeedbackData(self):
         pass
