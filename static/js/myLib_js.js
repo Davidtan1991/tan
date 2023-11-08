a1ee0d0bae87747254b7973be98c1c4c3c19ce49
FinnGlobal <finn.he0102@gmail.com>
2023-11-08 22:46:25 +1300

    js/jQuery start

---
+++ static/js/myLib_js.js
@@ -0,0 +1,35 @@
+// function getData() {
+ 
+// var btn = document.getElementById('btn');
+// btn.onclick = function () {
+//     var content = GetData(); //document.getElementById("table");
+//     var htmlBody = content ;
+//     toExcel(htmlBody);
+// }
+
+
+
+
+
+
+
+
+
+
+// function tidyData() {
+//     var mytable = document.getElementById("table");
+//     var data = "";
+//     for (var i = 0, rows = mytable.rows.length; i < rows; i++) {
+//         for (var j = 0, cells = mytable.rows[i].cells.length; j <= cells; j++) {
+//             if (j == cells) {      
+//                 console.log(j);
+//                 data = data + '\n';  
+//             }
+//             else {
+//                 data = data + mytable.rows[i].cells[j].innerHTML.toString() +"\t"+ ',';  
+//             };
+//         };
+//     };
+//     return data;
+// };
+
